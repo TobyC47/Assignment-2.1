96b039668084ded925910c7d267c951b8bedbc32
Toby Cockreham <toby.cockreham@hastings.edu>
2021-11-07 00:29:06 -0500

    Added code to calculate function. Mostly works but I got a different decimal point.

--- Assignment 2.1/Calculate.cs
+++ Assignment 2.1/Calculate.cs
@@ -8,9 +8,23 @@ namespace Assignment_2._1
 {
     class Calculate
     {
-        public static double MonPayment(double dInitPayment, double dAnnualIntRate, int iTerm)
+        public static double MonPayment(double dInitPrincipal, double dAnnualIntRate, int iTerm)
         {
+            double dMonPayment = 0;
+            int iTermMonths = iTerm * 12;
+            double dMonthlyIntRate = (dAnnualIntRate / 100) / 12;
+            double dPower = (dMonthlyIntRate) + 1;
+            double dStartingMonthlyRate = (dMonthlyIntRate) + 1;
 
+
+            for (int iMonth = 1; iMonth <= iTermMonths; iMonth++)
+            {
+                dPower = dPower * dStartingMonthlyRate;
+            }
+
+            dMonPayment = ((dMonthlyIntRate * dPower) / (dPower - 1)) * dInitPrincipal;
+
+            return dMonPayment;
         }
     }
 }
