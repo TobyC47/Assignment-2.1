19e1d232fdc044e034552429eaa4c0b274d765f2
Toby Cockreham <toby.cockreham@hastings.edu>
2021-11-08 19:13:49 -0600

    Finished!

--- MonthlyPaymentCalc/Calculate.cs
+++ MonthlyPaymentCalc/Calculate.cs
@@ -4,23 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assignment_2._1
+namespace MonthlyPaymentCalc
 {
-    class Calculate
+    public class Calculate
     {
         public static double MonPayment(double dInitPrincipal, double dAnnualIntRate, int iTerm)
         {
             double dMonPayment = 0;
             int iTermMonths = iTerm * 12;
             double dMonthlyIntRate = (dAnnualIntRate / 100) / 12;
-            double dPower = (dMonthlyIntRate) + 1;
-            double dStartingMonthlyRate = (dMonthlyIntRate) + 1;
-
-
-            for (int iMonth = 1; iMonth <= iTermMonths; iMonth++)
-            {
-                dPower = dPower * dStartingMonthlyRate;
-            }
+            double dPower = Math.Pow(dMonthlyIntRate + 1, iTermMonths);
 
             dMonPayment = ((dMonthlyIntRate * dPower) / (dPower - 1)) * dInitPrincipal;
 
